33d990b145dd9164a4fae8d7706925a403660ec8
Atul-Pratap-singh <atulpratap4784@gmail.com>
2025-04-26 15:29:52 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,11 @@
+# Basic Python Program: Addition of Two Numbers
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum)
